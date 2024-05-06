402455988b7525797a7b503d953d0da96d7bfffa
Sunset Mikoto <26019675+lwd-temp@users.noreply.github.com>
2024-05-06 09:45:38 +0000

    works now

--- patch_code.sh
+++ patch_code.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-file_path="clients/apps/browser/src/autofill/browser/main-context-menu-handler.ts"
-temp_dir="/tmp"
-temp_file="$temp_dir/main-context-menu-handler.ts"
-patch_file="RemoveCheckPremiumAccess.patch"
+patch_file="fakePremium.patch"
+repo_dir="clients"
 
-cp "$file_path" "$temp_file"
+cp "$patch_file" "$repo_dir"
 
-# sed -i 's/^\(\s*\)checkPremiumAccess: true,\s*$/\1\/\/checkPremiumAccess: true,/' "$file_path"
-patch "$file_path" < "$patch_file"
+cd "$repo_dir"
 
-diff "$file_path" "$temp_file"
+git apply "$patch_file"
