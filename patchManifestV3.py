3db7c0fce49cfebdcf71a44cb5c5fb0caf50c0d4
Sunset Mikoto <26019675+lwd-temp@users.noreply.github.com>
2024-05-22 22:44:01 +0800

    orig["applications"]["gecko"]["id"]

--- patchManifestV3.py
+++ patchManifestV3.py
@@ -9,6 +9,7 @@ orig["author"] = "Sunset Edu. & Tech. Group and Bitwarden Inc."
 orig["homepage_url"] = "https://github.com/lwd-temp/bitwarden-browser-extension-builder"
 orig["action"]["default_title"] = "Sunsetwarden"
 orig["sidebar_action"]["default_title"] = "Sunsetwarden"
+orig["applications"]["gecko"]["id"] = "{096091b7-0c7a-4e9e-a7c2-b9aed40cd430}"
 
 with open("clients/apps/browser/src/manifest.v3.json", "w", encoding="utf-8") as f:
     json.dump(orig, f, indent=2)
