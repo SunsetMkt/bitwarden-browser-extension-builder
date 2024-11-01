a60057f31200cc5b821e9a787d5c21d45f0b804d
Sunset <26019675+SunsetMkt@users.noreply.github.com>
2024-11-01 12:30:16 +0800

    New icon

--- patches/patchManifestV2.py
+++ patches/patchManifestV2.py
@@ -6,7 +6,9 @@ with open("clients/apps/browser/src/manifest.json", "r", encoding="utf-8") as f:
 orig["name"] = "Sunsetwarden"
 orig["short_name"] = "Sunsetwarden"
 orig["author"] = "Sunset Edu. & Tech. Group and Bitwarden® community"
-orig["homepage_url"] = "https://github.com/SunsetMkt/bitwarden-browser-extension-builder"
+orig["homepage_url"] = (
+    "https://github.com/SunsetMkt/bitwarden-browser-extension-builder"
+)
 orig["browser_action"]["default_title"] = "Sunsetwarden"
 orig["sidebar_action"]["default_title"] = "Sunsetwarden"
 del orig["applications"]["gecko"]["id"]
@@ -14,11 +16,10 @@ orig["description"] = (
     "Sunsetwarden is an exclusive build of the popular password manager Bitwarden®, designed for internal use by SETG."
 )
 orig["icons"] = {
-    "16": "images/icon16_gray.png",
-    "32": "images/icon32_gray.png",
-    "48": "images/icon48_gray.png",
-    "96": "images/icon96_gray.png",
-    "128": "images/icon128_gray.png",
+    "16": "images/icon16_sunset.png",
+    "32": "images/icon32_sunset.png",
+    "48": "images/icon48_sunset.png",
+    "128": "images/icon128_sunset.png",
 }
 
 if len(orig["description"]) > 132:
